5565e9e5443828a861531a50200e9a691bd0faf0
codewithashim <codewithashim@gmail.com>
2023-07-07 00:53:27 +0600

    done all

--- src/app.js
+++ src/app.js
@@ -3,13 +3,12 @@ const path = require("path");
 const app = express();
 require("./db/conn");
 const hbs = require("hbs");
-
-const Signin = require("./models/sigins");
-const async = require("hbs/lib/async");
-// const async = require("hbs/lib/async");
-// const {json} =require(express);
-
-const port = process.env.PORT || 3000;
+const crypto = require("crypto");
+const session = require("express-session");
+const authRouter = require("./modules/auth/auth.router");
+const tornamentRouter = require("./modules/tonaments/tonaments.router");
+const paymentRouter = require("./modules/payment/payment.router");
+const port = process.env.PORT || 5000;
 
 const static_path = path.join(__dirname, "../public");
 const template_path = path.join(__dirname, "../templates/views");
@@ -24,77 +23,63 @@ app.set("views", template_path);
 hbs.registerPartials(partials_path);
 
 app.get("/", (req, res) => {
-    res.render("index");
+  res.render("index");
 });
 
 app.get("/login", (req, res) => {
-    res.render("login");
+  res.render("login");
 });
 app.get("/booking", (req, res) => {
-    res.render("booking");
+  res.render("booking");
 });
 
 app.get("/signin", (req, res) => {
-    res.render("signin");
+  res.render("signin");
 });
-
-app.post("/signin", async (req, res) => {
-    try {
-        console.log(req.body);
-        const email = req.body.email;
-        const password = req.body.password;
-        const username = req.body.username;
-
-        const signinUser = new Signin({
-            name: username,
-            email: email,
-            password: password,
-        });
-
-        await signinUser.save();
-        console.log(signinUser);
-        res.status(201).render("./index")
-    } catch (error) {
-        res.status(400).send(error);
-    }
-});
-
-// for login
-
-app.post("/login", async (req, res) => {
-    try {
-        const email = req.body.email;
-        const password = req.body.password;
-
-        const useremail = await Signin.findOne({email:email});
-
-        if (useremail.password === password) {
-            res.status(201).render("./index")
-        } else {
-            res.status(400).send("your password and email are incorrect");
-        }
-
-    } catch (error) {
-        res.status(400).send("invalid Email")
-    }
-});
-
 app.get("/tournamentpage", (req, res) => {
-    res.render("tournamentpage");
+  res.render("tournamentpage");
 });
 
 app.get("/cart", (req, res) => {
-    res.render("cart");
+  res.render("cart");
 });
 
 app.get("/step3", (req, res) => {
-    res.render("step3");
+  res.render("step3");
 });
 
 app.get("/payment", (req, res) => {
-    res.render("payment");
+  res.render("payment");
 });
 
+///////////////////////////////////////////////
+//                Middelwaire               //
+/////////////////////////////////////////////*/
+
+const generateSecretKey = () => {
+  return crypto.randomBytes(32).toString("hex");
+};
+
+const secretKey = generateSecretKey();
+console.log(secretKey);
+
+// Set up session middleware
+app.use(
+  session({
+    secret: secretKey,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+app.use(authRouter);
+app.use(tornamentRouter);
+app.use(paymentRouter);
+
+///////        End of Middelwaire     ////////
+
 app.listen(port, () => {
-    console.log(`server is running ${port}`);
+  console.log(`server is running ${port}`);
 });
+
+module.exports = app;
